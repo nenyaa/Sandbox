220772ead143741d078f68f7149b8a7c8295701e
nenyaa <139075287+nenyaa@users.noreply.github.com>
2023-07-27 11:06:47 +0200

    Add API task.

---
+++ ALX_python/89_1 API Chuck Norris joke.py
@@ -0,0 +1,13 @@
+import requests
+print("### Żarty Chucka Norisa ###\nWskaż jedną z kategorii:")
+
+dane = requests.get("https://api.chucknorris.io/jokes/categories")
+x = dane.json()
+for i in x:
+    print(i, end =" ")
+print("")
+wybor = input()
+
+zart = requests.get(f"https://api.chucknorris.io/jokes/random?category={wybor}")
+zart = zart.json()
+print(zart["value"])
