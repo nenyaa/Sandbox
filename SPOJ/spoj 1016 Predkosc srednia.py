f303a5e2ea0aa3b764c8ed2ecb71758572735b2d
nenyaa <139075287+nenyaa@users.noreply.github.com>
2023-07-27 10:27:57 +0200

    Code refactoring.

--- SPOJ/spoj 1016 Predkosc srednia.py
+++ SPOJ/spoj 1016 Predkosc srednia.py
@@ -1,9 +1,7 @@
 i = int(input())
 lista = []
 for x in range(i):
-    v, v2 = input().split()
-    v = int(v)
-    v2 = int(v2)
+    v, v2 = map(int, input().split())
     srednia = (2 * v * v2) / (v + v2)
     srednia = int(srednia)
     lista.append(srednia)
