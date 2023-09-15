a6d1648a566bea9b5a503c6bd758b0f003d662ff
nenyaa <139075287+nenyaa@users.noreply.github.com>
2023-09-15 12:45:20 +0200

    Add spoj task no 2598 solution.

---
+++ SPOJ/spoj 2598 Kabalistyczny zapis daty.py
@@ -0,0 +1,10 @@
+d = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "k": 10, "l": 20,
+     "m": 30, "n": 40, "o": 50, "p": 60, "q": 70, "r": 80, "s": 90, "t": 100, "v": 200,
+     "x": 300, "y": 400, "z": 500}
+
+data = input()
+counter = 0
+for letter in data:
+    if letter in d:
+        counter += d[letter]
+print(counter)
