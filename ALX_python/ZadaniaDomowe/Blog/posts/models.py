1b5704cbb73473d26ccabcf2abdb0807bd625e2c
nenyaa <139075287+nenyaa@users.noreply.github.com>
2023-12-01 14:44:24 +0100

    Add verbose_name to Post model.

--- ALX_python/ZadaniaDomowe/Blog/posts/models.py
+++ ALX_python/ZadaniaDomowe/Blog/posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    title = models.CharField(verbose_name="tytuł", max_length=255)
+    content = models.TextField(verbose_name="treść")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
